7f06e415af35bd8efd56e6d2369afe1edbd311df
Kanofi <lukakanofi@gmail.com>
2025-11-25 23:29:21 -0500

    finished desinging ui

---
+++ script.js
@@ -0,0 +1,6 @@
+// Things we will add on js
+// 1. Getting wether data ( fetching data) 
+// 2. Accepting input from the city
+// 3. Displaying the data
+// 4. Persistent file storge for favourite cities
+// 5. Toggle for light and dark mode
