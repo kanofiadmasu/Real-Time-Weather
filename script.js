a5cdee3a7a174583ce99fbbd682bb22b3e5cdcce
Kanofi <lukakanofi@gmail.com>
2025-11-29 11:45:55 -0500

    Implemented proper UI design

--- script.js
+++ script.js
@@ -4,3 +4,11 @@
 // 3. Displaying the data
 // 4. Persistent file storge for favourite cities
 // 5. Toggle for light and dark mode
+
+
+// const form = document.querySelector('.input-form');
+// const dynamicSection = document.querySelector('.dynamic-section');
+// const saveButton = document.querySelector('.fav-city');
+// const themeToggel = document.querySelector('.night-mode');
+// const apiKey = "362ca154ca2ba88b090edbf69c89e4f1"
+
